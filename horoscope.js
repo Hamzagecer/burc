e329732704f3c3d2941b8584f73bb157fa106655
Hamzagecer <etemmmete14@gmail.com>
2021-02-17 01:07:08 +0100

    first commit

---
+++ horoscope.js
@@ -0,0 +1,129 @@
+let gun = 9
+let ay = 6
+
+   if ( gun >= 21 && gun <= 31 && ay == 3){
+
+    console.log("koc")
+}
+ else if(gun <= 20 && gun >= 1 && ay == 4){ 
+
+    console.log("koc")
+
+ }
+
+ if (gun >= 21 && gun <= 30 && ay == 4){
+
+    console.log("boga")
+}
+ else if( gun <=20 && gun >= 1 && ay == 5){
+
+    console.log("boga")
+
+ }
+ if (gun <= 21 && gun >= 1 &&  ay == 6){
+
+    console.log("ikizler ve hamza gecerin burcudur")
+}
+ else if( gun >= 21 && gun <= 31 && ay == 5){
+
+    console.log("ikizler ve hamza gecerin burcudur")
+
+ }
+
+ if (gun >= 21 && gun <= 30 &&  ay == 6){
+
+    console.log("yengec")
+}
+ else if( gun <= 21 && gun >= 1 && ay == 7){
+
+    console.log("yengec")
+
+ }
+
+ if (gun >= 23 && gun <= 31 && ay  == 7){
+
+    console.log("aslan")
+}
+ else if( gun <= 22 && gun >= 1 && ay == 8){
+
+    console.log("aslan")
+
+ }
+
+
+if (gun >= 23 && gun <= 31 && ay == 8){
+
+    console.log("basak")
+}
+ else if( gun <= 22 &&  gun >= 1 && ay == 9){
+
+    console.log("basak")
+
+ }
+
+ if (gun >= 23 && gun <= 30 && ay == 9){
+
+    console.log("terazi")
+}
+ else if( gun <= 23 && gun >= 1 && ay == 10){
+
+    console.log("terazi")
+
+ }
+ if (gun >= 24 && gun <= 31 &&  ay == 10){
+
+    console.log("akrep")
+}
+ else if( gun <= 22 &&  gun >= 1 && ay == 11){
+
+    console.log("akrep")
+
+ }
+ 
+ if (gun >= 23 && gun <= 30 && ay == 11){
+
+    console.log("yay")
+}
+ else if( gun <= 21 && gun >= 1 && ay == 12){
+
+    console.log("yay")
+
+ }
+ 
+
+ if (gun >= 22 && gun <= 31 && ay == 12){
+
+    console.log("oglak")
+}
+ else if( gun <= 20 && gun >= 1 && ay == 1){
+
+    console.log("oglak")
+
+ }
+ 
+
+ if (gun >= 21 && gun <= 31 &&  ay == 1){
+
+    console.log("kova")
+}
+ else if( gun <= 18 &&  gun >= 1 && ay == 2){
+
+    console.log("kova")
+
+ }
+ 
+
+ if (gun >= 19 &&  gun <= 28 && ay == 2){
+
+    console.log("balik")
+}
+ else if( gun <= 20 && gun >= 1 &&  ay == 3){
+
+    console.log("balik")
+
+ }
+
+
+ // en sona else ile ''yanlis girdiniz'' notunu da yazdim fakat her sonucun arkasinda bu yaziyi da verdigi icin bunu kaldirdim. 
+ 
+
